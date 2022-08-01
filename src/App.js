1ec3bebad28f8c4d256118ad7ebfb3c8c02b39e3
Govindskatyura <govindskatyura@gmail.com>
2022-08-01 07:42:54 +0530

    restructure

--- src/App.js
+++ src/App.js
@@ -8,80 +8,11 @@ import "./App.css";
 import FileUpload from "./components/FileUpload";
 import { useForm } from "react-hook-form";
 import { Box, Grid } from "@chakra-ui/react";
-import Tablebox from "./components/Tablebox";
+import Sql from "./components/Sql";
 
 const defaultLanguage = "sql";
 const defaultTheme = Object.keys(themes).sort()[0];
 
-function getColumnNames(input) {
-  return (/\bselect\b\s+([\S\s]+?)from/i.exec(input) || [, ""])[1].split(
-    /\s*,\s*/g
-  );
-}
-
-function getTableNames(input) {
-  // function takes a sql query and returns an array of table names
-  // var aliases = sql.match(/(?<=\b(?:from|join) )\w+(?: (?!\bjoin\b)\w+)?/g);
-  var aliases = input.match(/(?<=\b(?:from|join) )\w+(?: (?!\bjoin\b)\w+)?/g);
-  if (aliases === null) {
-    return [];
-  }
-  var TableMap = {};
-  aliases.forEach((alias) => {
-    var splited = alias.trim().split(" ");
-    if (splited.length === 1) {
-      TableMap[alias.toLowerCase()] = alias.toLowerCase();
-    } else {
-      TableMap[splited[0].toLowerCase()] = splited[1].toLowerCase();
-    }
-  });
-  return TableMap;
-}
-
-function random(number) {
-  return Math.floor(Math.random() * number);
-}
-function randomColor() {
-  return "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
-}
-
-// sql query parser
-function Sql(key, text,colors,handlesetColors) {
-  var columns = getColumnNames(text);
-  var tables = getTableNames(text);
-  var alias = {};
-  for (var table in tables) {
-    alias[tables[table]] = table;
-  }
-  // make a random color for each table as a dictionary
-  Object.keys(tables).forEach((table) => {
-    if(!colors[table]){
-    handlesetColors(table,randomColor());
-    }
-   }
-  );
-  var total_tables = Object.keys(tables).length;
-  var first_table = Object.keys(tables)[0];
-  return (
-    <div
-      style={{ display: "flex", marginBottom: "4px" }}
-      className="arrow-steps clearfix"
-    >
-      {columns.map((text, index) => {
-        if (
-          text.trim().toLowerCase() !== "select" &&
-          text.toLowerCase() !== ""
-        ) {
-          return (
-            <Box backgroundColor={colors[total_tables===1?first_table: alias[text.split(".")[0].toLowerCase()]]} key={index} color="#fff" className="step">
-              {text}
-            </Box>
-          );
-        } else return <></>;
-      })}
-    </div>
-  );
-}
 
 export default function App() {
   const [input, setInput] = useState("");
